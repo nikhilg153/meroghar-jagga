943bb0217ab63b0e84eff12490fe6a7141460337
Nikhil Gupta <nikhilgupta@Nikhils-MacBook-Air.local>
2025-07-11 10:23:58 +0545

    add radix ui to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import { Theme } from "@radix-ui/themes";
+import { Flex, Text, Button } from "@radix-ui/themes";
 
 export default function Home() {
-  return <div>Meroghar Jagga</div>;
+  return (
+    <Theme>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :)</Text>
+        <Button>Let's go</Button>
+      </Flex>
+    </Theme>
+  );
 }
